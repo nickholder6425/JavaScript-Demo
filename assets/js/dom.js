ffcce66a0e87d410781da593917139d118a91f32
nicholasholder <ngholder@hotmail.com>
2023-11-20 17:21:33 -0600

    modified js and index.html

---
+++ assets/js/dom.js
@@ -0,0 +1,31 @@
+// EXAMINES THE DOCUMENT OBJECT
+// console.dir(document);
+
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// // document.title = 123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[11]);
+// // document.all[11].textContent ='Hello';
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+
+// getElementById
+// console.log(document.getElementById('header-title'));
+var headerTitle=document.getElementById('header-title');
+console.log(headerTitle);
+// headerTitle.textContent='Hello';
+// headerTitle.innerText='Goodbye';
+// headerTitle.textContent
+// console.log(headerTitle.innerText);
+headerTitle.innerHTML = '<h3>Hello</h3';
+
+
+
+
